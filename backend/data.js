3a86d01d2cc7afceffe606db118c3207833f7daa
King Portipher <kingportipher@gmail.com>
2020-11-17 21:53:31 +0000

    FrontEnd Done

---
+++ backend/data.js
@@ -0,0 +1,35 @@
+export default {
+    products: [{
+        _id:'1',
+        name: "Apple Laptop",
+        category: "Laptop",
+        image: '/images/apple1.jpeg',
+        price: 1999.99,
+        brand:'Apple',
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        _id:'2',
+        name: "Apple iPhone",
+        category: "Phones",
+        image: '/images/apple1.jpeg',
+        price: 999,
+        brand:'Apple',
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        _id:'3',
+        name: "Apple iMac",
+        category: "Desktop",
+        image: '/images/apple1.jpeg',
+        price: 4999.99,
+        brand:'Apple',
+        rating: 4.5,
+        numReviews: 10
+    },
+
+    
+]
+}
