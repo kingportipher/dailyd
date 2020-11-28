27825e243c44a25814e7410279ca1169e101bdb2
King Portipher <kingportipher@gmail.com>
2020-11-28 11:23:28 +0000

    final code

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export default {
+    MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://king:00233K@cluster0.c3all.mongodb.net/dailydeals?'
+ 
+}
--- backend/data.js
+++ backend/data.js
@@ -7,27 +7,63 @@ export default {
         price: 1999.99,
         brand:'Apple',
         rating: 4.5,
-        numReviews: 10
+        numReviews: 10,
+        countInStock:6
     },
     {
         _id:'2',
         name: "Apple iPhone",
         category: "Phones",
-        image: '/images/apple1.jpeg',
+        image: '/images/iphone.jpg',
         price: 999,
         brand:'Apple',
         rating: 4.5,
-        numReviews: 10
+        numReviews: 10,
+        countInStock:6
     },
     {
         _id:'3',
         name: "Apple iMac",
         category: "Desktop",
-        image: '/images/apple1.jpeg',
+        image: '/images/imac.jpg',
         price: 4999.99,
         brand:'Apple',
         rating: 4.5,
-        numReviews: 10
+        numReviews: 10,
+        countInStock:6
+    },
+    {
+        _id:'4',
+        name: "Apple iMac",
+        category: "Desktop",
+        image: '/images/imac1.jpeg',
+        price: 4999.99,
+        brand:'Apple',
+        rating: 4.5,
+        numReviews: 10,
+        countInStock:6
+    },
+    {
+        _id:'5',
+        name: "Apple Tablet",
+        category: "Desktop",
+        image: '/images/ipad.jpg',
+        price: 699.99,
+        brand:'Apple',
+        rating: 4.5,
+        numReviews: 8,
+        countInStock:6
+    },
+    {
+        _id:'6',
+        name: "Apple Phone",
+        category: "Phones",
+        image: '/images/iphone.jpg',
+        price: 1029.99,
+        brand:'Apple',
+        rating: 4.5,
+        numReviews: 10,
+        countInStock:6
     },
 
     
